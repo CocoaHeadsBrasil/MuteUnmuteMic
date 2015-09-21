1408b043d7710d79ae009e574c41d20aaa20818b
Gustavo Barbosa <gustavocsb@gmail.com>
2015-09-21 09:42:36 -0400

    set copyright to cocoaheads brasil

--- MuteUnmuteMic/main.m
+++ MuteUnmuteMic/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  MuteUnmuteMic
-//
-//  Created by Gustavo Barbosa on 9/20/15.
-//  Copyright © 2015 Gustavo Barbosa. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
