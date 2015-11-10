e4c1e310df75c519f76855189370b36265f61f42
Raphael Silva <sr.silvaraphael@gmail.com>
2015-11-10 15:31:42 -0200

    App name changed to [Un]MuteMic. All references were updated.

--- MuteUnmuteMic/AppDelegate.m
+++ MuteUnmuteMic/AppDelegate.m
@@ -39,7 +39,7 @@ static NSInteger const kDefaultVolume = 70;
     
     NSStatusItem *menuItem =
     [statusBar statusItemWithLength:NSVariableStatusItemLength];
-    [menuItem setToolTip:@"Un[Mute]Mic by CocoaHeads Brazil"];
+    [menuItem setToolTip:@"[Un]MuteMic by CocoaHeads Brazil"];
     [menuItem setImage:[NSImage imageNamed:@"mic_on"]];
     [menuItem setHighlightMode:YES];
 
